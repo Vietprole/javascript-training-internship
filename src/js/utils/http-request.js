50ba84a16b113363d6656d91e80815a936874593
Vietprole <phuvietdn@gmail.com>
2024-09-16 14:07:54 +0700

    Rewrite httpRequest() add async/await

--- src/js/utils/http-request.js
+++ src/js/utils/http-request.js
@@ -6,23 +6,16 @@ async function httpRequest(method, body = null, url = API_BASE_URL) {
     options.headers = { 'Content-Type': 'application/json' };
     options.body = body;
   }
-  fetch(url, options)
-    .then((response) => {
-      // Check if the response is OK (status code 200-299)
-      if (!response.ok) {
-        throw new Error(`Network response was not ok ${response.statusText}`);
-      }
-      // Parse the JSON from the response
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the data from the response
-      console.log(data);
-    })
-    .catch((error) => {
-      // Handle any errors that occurred during the fetch
-      console.error('There was a problem with the fetch operation:', error);
-    });
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
 }
 
 export default httpRequest;
