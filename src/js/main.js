fb088d2341606d56e500d26c27b41155f684bbf3
Vietprole <phuvietdn@gmail.com>
2024-09-23 14:03:34 +0700

    Remove dashboard feature

--- src/js/main.js
+++ src/js/main.js
@@ -29,130 +29,6 @@ class Customer {
 
 export default Customer;
 
-// Function to remove all table-row div elements
-function removeAllTableRows() {
-  const tableRows = document.querySelectorAll('.table-row');
-  tableRows.forEach((row) => row.remove());
-}
-
-// Get customers to view on Dashboard
-// TODO - This should be improved after implementing the Add customer functionality
-function LoadCustomers() {
-  removeAllTableRows();
-  httpRequest(HTTP_METHODS.GET).then((customers) => {
-    customers.forEach((customer) => {
-      // Create a new table row
-      const newRow = document.createElement('div');
-      newRow.classList.add('table-row');
-
-      // Create a name cell
-      const nameCell = document.createElement('div');
-      nameCell.classList.add('name-cell');
-      // Create a name div
-      const name = document.createElement('div');
-      name.classList.add('name');
-      name.textContent = customer.name;
-      // Create a id div
-      const id = document.createElement('div');
-      id.classList.add('id');
-      id.textContent = customer.id;
-      // Append name and id divs to name cell
-      nameCell.appendChild(name);
-      nameCell.appendChild(id);
-
-      // Create a description cell
-      const descriptionCell = document.createElement('div');
-      descriptionCell.classList.add('description-cell');
-      descriptionCell.textContent = customer.description;
-
-      // Create a status cell
-      const statusCell = document.createElement('div');
-      statusCell.classList.add('status-cell');
-      const { status } = customer;
-      // Add class based on status
-      statusCell.classList.add(`status-${status.toLowerCase()}`);
-      statusCell.textContent = status;
-
-      // Create a reusable dollar sign element
-      const dollarSign = document.createElement('span');
-      dollarSign.textContent = '$';
-
-      // Create a reusable currency element
-      const currency = document.createElement('div');
-      currency.classList.add('currency');
-      currency.textContent = 'CAD';
-
-      // Create a rate cell
-      const rateCell = document.createElement('div');
-      rateCell.classList.add('rate-cell');
-      // Create a rate amount div
-      const rateAmount = document.createElement('div');
-      rateAmount.classList.add('amount');
-      const rateDollarSign = dollarSign.cloneNode(true); // Clone the dollar sign element
-      const rate = document.createElement('span');
-      rate.textContent = customer.rate;
-      rateAmount.appendChild(rateDollarSign);
-      rateAmount.appendChild(rate);
-      // Create a rate currency div
-      const rateCurrency = currency.cloneNode(true); // Clone the currency element
-      rateCell.appendChild(rateAmount);
-      rateCell.appendChild(rateCurrency);
-
-      // Create a balance cell
-      const balanceCell = document.createElement('div');
-      balanceCell.classList.add('balance-cell');
-      // Create a balance amount div
-      const balanceAmount = document.createElement('div');
-      balanceAmount.classList.add('amount');
-      const balanceDollarSign = dollarSign.cloneNode(true); // Clone the dollar sign element
-      const balance = document.createElement('span');
-      balance.textContent = customer.balance;
-      balanceAmount.appendChild(balanceDollarSign);
-      balanceAmount.appendChild(balance);
-      // Create a balance currency div
-      const balanceCurrency = currency.cloneNode(true); // Clone the currency element
-      balanceCell.appendChild(balanceAmount);
-      balanceCell.appendChild(balanceCurrency);
-
-      // Create a deposit cell
-      const depositCell = document.createElement('div');
-      depositCell.classList.add('deposit-cell');
-      // Create a deposit amount div
-      const depositAmount = document.createElement('div');
-      depositAmount.classList.add('amount');
-      const depositDollarSign = dollarSign.cloneNode(true); // Clone the dollar sign element
-      const deposit = document.createElement('span');
-      deposit.textContent = customer.deposit;
-      depositAmount.appendChild(depositDollarSign);
-      depositAmount.appendChild(deposit);
-      // Create a deposit currency div
-      const depositCurrency = currency.cloneNode(true); // Clone the currency element
-      depositCell.appendChild(depositAmount);
-      depositCell.appendChild(depositCurrency);
-
-      // Create a menu button
-      const menuButton = document.createElement('div');
-      menuButton.classList.add('menu-button', 'icon-wrapper');
-      const menuImg = document.createElement('img');
-      menuImg.src = menuIcon;
-      menuImg.alt = 'Menu icon';
-      menuButton.appendChild(menuImg);
-
-      // Append all cells to the new row
-      newRow.appendChild(nameCell);
-      newRow.appendChild(descriptionCell);
-      newRow.appendChild(statusCell);
-      newRow.appendChild(rateCell);
-      newRow.appendChild(balanceCell);
-      newRow.appendChild(depositCell);
-      newRow.appendChild(menuButton);
-
-      const tableHeader = document.querySelector('.table-header');
-      tableHeader.insertAdjacentElement('afterend', newRow);
-    });
-  });
-}
-LoadCustomers();
 // const newUuid = uuidv4();
 // const newCustomer = new Customer(
 //   newUuid,
@@ -271,7 +147,7 @@ createButton.addEventListener('click', () => {
 
   // Send a POST request to the API
   httpRequest(HTTP_METHODS.POST, newCustomer.toJSON());
-  LoadCustomers();
+  // LoadCustomers();
   closeModal(addCustomerModal);
 });
 
