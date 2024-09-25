45460d4c11bc52d00daeededa16464ccc5f6bfc6
Vietprole <phuvietdn@gmail.com>
2024-09-25 13:54:44 +0700

    Remove unused code

--- src/js/main.js
+++ src/js/main.js
@@ -38,32 +38,6 @@ class Customer {
 
 export default Customer;
 
-// const newUuid = uuidv4();
-// const newCustomer = new Customer(
-//   newUuid,
-//   'John Boe',
-//   'Due',
-//   0.05,
-//   2000,
-//   600,
-//   'A new test customer'
-// );
-// httpRequest(HTTP_METHODS.POST, newCustomer.toJSON());
-
-// const putUrl = `${API_BASE_URL}/8434960a-b06b-4dc9-98b9-d6306de7cdad`;
-// const updatedCustomer = new Customer(
-//   newUuid,
-//   'John Doe',
-//   'Open',
-//   0.05,
-//   2000,
-//   600,
-//   'An updated test customer'
-// );
-
-// httpRequest(HTTP_METHODS.PUT, updatedCustomer.toJSON(), putUrl);
-// httpRequest(HTTP_METHODS.DELETE, null, `${API_BASE_URL}/5b4f0c09-6f57-4467-8cad-23c11a65afe1`);
-
 //* Action menu functionality
 const actionMenuButtons = document.querySelectorAll('.menu-button');
 const actionMenu = document.querySelector('.action-menu');
