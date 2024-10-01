c3cfa44aabd44e6c95d5f897c42b83298388e0b8
Vietprole <phuvietdn@gmail.com>
2024-10-01 09:37:31 +0700

    Implement edit customer feature

--- src/js/main.js
+++ src/js/main.js
@@ -65,6 +65,7 @@ const addButton = document.querySelector('.add-button');
 const editButton = document.querySelector('.edit-button');
 const viewButton = document.querySelector('.view-button');
 const createButton = document.querySelector('.create-button');
+const saveButton = document.querySelector('.save-button');
 // const saveButton = document.querySelector('.save-button');
 const closeAddModalButton = document.querySelector('.add-customer-modal .close-button');
 const closeEditModalButton = document.querySelector('.edit-customer-modal .close-button');
@@ -115,13 +116,12 @@ function closeModal(modal) {
   modalOverlay.classList.remove('open'); // Hide the overlay
 }
 
-//* Form validation
-// For Add customer modal
+//* For Add customer modal
 const addNameInput = document.getElementById('add-name-input');
 const addRateInput = document.getElementById('add-rate-input');
 const addBalanceInput = document.getElementById('add-balance-input');
 const addDepositInput = document.getElementById('add-deposit-input');
-// Function to check if all inputs are valid
+// Add form validation
 function checkAddFormValidity() {
   const isFormValid =
     addNameInput.value && addRateInput.value && addBalanceInput.value && addDepositInput.value;
@@ -136,9 +136,11 @@ let previousDepositValue = '';
 addNameInput.addEventListener('input', enforceMaxLength);
 addNameInput.addEventListener('input', checkAddFormValidity);
 addNameInput.addEventListener('blur', showErrorIfEmpty);
+
 addNameInput.addEventListener('keydown', function () {
   previousNameValue = this.value;
 });
+
 addNameInput.addEventListener('input', function () {
   if (this.value && hasNumbers(this.value)) {
     this.value = previousNameValue;
@@ -147,9 +149,11 @@ addNameInput.addEventListener('input', function () {
 
 addRateInput.addEventListener('input', checkAddFormValidity);
 addRateInput.addEventListener('blur', showErrorIfEmpty);
+
 addRateInput.addEventListener('keydown', function () {
   previousRateValue = this.value;
 });
+
 addRateInput.addEventListener('input', function () {
   if (this.value && !isValid(this.value)) {
     this.value = previousRateValue;
@@ -158,9 +162,11 @@ addRateInput.addEventListener('input', function () {
 
 addBalanceInput.addEventListener('input', checkAddFormValidity);
 addBalanceInput.addEventListener('blur', showErrorIfEmpty);
+
 addBalanceInput.addEventListener('keydown', function () {
   previousBalanceValue = this.value;
 });
+
 addBalanceInput.addEventListener('input', function () {
   // Allow negative numbers
   const regex = /^(-\d{0,7}(\.\d{0,2})?|\d{1,7}(\.\d{0,2})?)$/;
@@ -171,16 +177,18 @@ addBalanceInput.addEventListener('input', function () {
 
 addDepositInput.addEventListener('input', checkAddFormValidity);
 addDepositInput.addEventListener('blur', showErrorIfEmpty);
+
 addDepositInput.addEventListener('keydown', function () {
   previousDepositValue = this.value;
 });
+
 addDepositInput.addEventListener('input', function () {
   if (this.value && !isValid(this.value)) {
     this.value = previousDepositValue;
   }
 });
 
-//* For Add customer modal
+// Add customer modal open and close
 addButton.addEventListener('click', () => {
   openModal(addCustomerModal);
 });
@@ -189,6 +197,7 @@ closeAddModalButton.addEventListener('click', () => {
   closeModal(addCustomerModal);
 });
 
+// Add a new customer with create button
 createButton.addEventListener('click', async () => {
   const id = uuidv4();
   const name = document.getElementById('add-name-input').value;
@@ -205,13 +214,99 @@ createButton.addEventListener('click', async () => {
   const newCustomer = new Customer(id, name, status, rate, balance, deposit, description);
   // Send a POST request to the API
   await httpRequest(HTTP_METHODS.POST, newCustomer.toJSON());
-  closeModal(addCustomerModal);
   addNewTableRow(newCustomer);
+  closeModal(addCustomerModal);
 });
 
 //* For Edit customer modal
-editButton.addEventListener('click', () => {
+const editNameInput = document.getElementById('edit-name-input');
+const editStatusInput = document.getElementById('edit-status-input');
+const editRateInput = document.getElementById('edit-rate-input');
+const editBalanceInput = document.getElementById('edit-balance-input');
+const editDepositInput = document.getElementById('edit-deposit-input');
+const editDescriptionInput = document.getElementById('edit-description-input');
+// Edit form validation
+function checkEditFormValidity() {
+  const isFormValid =
+    editNameInput && editRateInput.value && editBalanceInput.value && editDepositInput.value;
+  saveButton.disabled = !isFormValid;
+}
+
+let previousEditRateValue = '';
+let previousEditBalanceValue = '';
+let previousEditDepositValue = '';
+
+editRateInput.addEventListener('input', checkEditFormValidity);
+editRateInput.addEventListener('keydown', function () {
+  previousEditRateValue = this.value;
+});
+editRateInput.addEventListener('input', function () {
+  if (this.value && !isValid(this.value)) {
+    this.value = previousEditRateValue;
+  }
+});
+
+editBalanceInput.addEventListener('input', checkEditFormValidity);
+editBalanceInput.addEventListener('keydown', function () {
+  previousEditBalanceValue = this.value;
+});
+editBalanceInput.addEventListener('input', function () {
+  // Allow negative numbers
+  const regex = /^(-\d{0,7}(\.\d{0,2})?|\d{1,7}(\.\d{0,2})?)$/;
+  if (this.value && !regex.test(this.value)) {
+    this.value = previousEditBalanceValue;
+  }
+});
+
+editDepositInput.addEventListener('input', checkEditFormValidity);
+editDepositInput.addEventListener('keydown', function () {
+  previousEditDepositValue = this.value;
+});
+
+editDepositInput.addEventListener('input', function () {
+  if (this.value && !isValid(this.value)) {
+    this.value = previousEditDepositValue;
+  }
+});
+
+// Fill the edit form with the current customer's data
+async function fillEditModal() {
+  const customer = await httpRequest(
+    HTTP_METHODS.GET,
+    null,
+    `${API_BASE_URL}/${currentCustomerID}`
+  );
+  editNameInput.value = customer.name;
+  editStatusInput.value = customer.status;
+  editRateInput.value = customer.rate;
+  editBalanceInput.value = customer.balance;
+  editDepositInput.value = customer.deposit;
+  editDescriptionInput.value = customer.description;
+
   openModal(editCustomerModal);
+}
+
+editButton.addEventListener('click', fillEditModal);
+
+// Edit current customer with save button
+saveButton.addEventListener('click', async () => {
+  const id = currentCustomerID;
+  const name = document.getElementById('edit-name-input').value;
+  const status = document.getElementById('edit-status-input').value;
+  let rate = document.getElementById('edit-rate-input').value;
+  let balance = document.getElementById('edit-balance-input').value;
+  let deposit = document.getElementById('edit-deposit-input').value;
+  const description = document.getElementById('edit-description-input').value;
+  // Remove any trailing decimal points or negative signs
+  rate = sanitizeInput(rate);
+  balance = sanitizeInput(balance);
+  deposit = sanitizeInput(deposit);
+  // Create an updated Customer instance
+  const updatedCustomer = new Customer(id, name, status, rate, balance, deposit, description);
+  // Send a POST request to the API
+  await httpRequest(HTTP_METHODS.PUT, updatedCustomer.toJSON(), `${API_BASE_URL}/${id}`);
+  loadCustomers();
+  closeModal(editCustomerModal);
 });
 
 closeEditModalButton.addEventListener('click', () => {
